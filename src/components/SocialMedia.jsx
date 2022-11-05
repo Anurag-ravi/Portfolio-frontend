6ce1be69469131bce8be7354a4c7a68ccec33102
Anurag Ravi <kumarayush1014@gmail.com>
2022-11-05 18:54:52 +0530

    added photo and logo

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -6,7 +6,7 @@ const SocialMedia = () => {
   return (
     <div className='app__social'>
         <div><a href='https://twitter.com/AnuragRavi_' target='_blank'><BsTwitter /></a></div>
-        <div><a href='https://www.facebook.com/ayushsrivasrav1916' target='_blank'><FaFacebookF /></a></div>
+        <div><a href='https://www.facebook.com/anuragravii/' target='_blank'><FaFacebookF /></a></div>
         <div><a href='https://www.instagram.com/anu.rag__r/' target='_blank'><BsInstagram /></a></div>
     </div>
   )
